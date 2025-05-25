6bfd5478759fa1d24b415d8e688c2a4978cbd2fd
Vivek Jha <vivek.ajay.p50@gmail.com>
2025-05-26 02:43:27 +0530

    Database Added

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,47 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-
+import { drizzle } from "drizzle-orm/singlestore";
+import { createPool, type Pool } from "mysql2/promise";
 import { env } from "~/env";
 import * as schema from "./schema";
+
+//! Connection Pool to a SingleStore Database ===>> Using mysql2/promise driver
+//! Then using drizzle ORM to bind this connection to our SingleStore database schema
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
+
+/**
+ *
+ * A connection pool:
+ * - Is a cache of open database connections.
+ * - Prevents the overhead of creating a new connection for every query.
+ * - Handles multiple queries efficiently using a pool of reusable connections.
+ *
+ * Here, mysql2/promise handles the pool for you under the hood.
+ *
+*/
+
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  conn: Pool | undefined;
 };
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+const conn = globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASSWORD,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  });
 
-export const db = drizzle(client, { schema });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+conn.addListener("error", (err) => {
+  console.error("Database connection error: ", err)
+});
+
+
+export const db = drizzle(conn, { schema });
