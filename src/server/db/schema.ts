41db5cad929a03375b8fd96a59adedf96a2e8af1
Vivek Jha <vivek.ajay.p50@gmail.com>
2025-05-29 18:38:20 +0530

    DB Model Working\!

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,25 @@
-import { int, bigint, text, singlestoreTable, date, boolean } from "drizzle-orm/singlestore-core";
+import { int, text, index, singlestoreTableCreator, bigint } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
+
+export const createTable = singlestoreTableCreator((name) => `my_drive_${name}`)
+
+
+export const files = createTable("files_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  type: text("type").notNull(),
+  url: text("url").notNull(),
+  size: int("size").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+}, (t) => {
+  return [index("parent_index").on(t.parent)]
 });
+
+export const folders = createTable("folders_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  type: text("type"),
+  parent: bigint("parent", { mode: "number", unsigned: true }),
+}, (t) => {
+  return [index("parent_index").on(t.parent)]
+})
